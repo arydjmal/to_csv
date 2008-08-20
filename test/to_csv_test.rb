e718e58cac8e485a815e279ed01236106ee1f4fe
Ary Djmal <arydjmal@gmail.com>
2008-08-19 20:47:14 -0400

    added more tests

--- test/to_csv_test.rb
+++ test/to_csv_test.rb
@@ -1,8 +1,31 @@
 require 'test/unit'
+require 'rubygems'
+require 'fastercsv'
+require File.dirname(__FILE__) + '/../lib/to_csv'
+require File.dirname(__FILE__) + '/user_model'
 
 class ToCsvTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  # def test_this_plugin
-  #   flunk
-  # end
+
+  def setup
+    @users = []
+    @users << User.new(:id => 1, :name => 'Ary', :age => 24)
+    @users << User.new(:id => 2, :name => 'Nati', :age => 21)
+  end
+
+  def test_with_no_options
+    assert_equal( "name,age\nAry,24\nNati,21\n", @users.to_csv )
+  end
+  
+  def test_with_id
+    assert_equal( "id,name,age\n1,Ary,24\n2,Nati,21\n", @users.to_csv(:id => true) )
+  end
+  
+  def test_with_only
+    assert_equal( "name\nAry\nNati\n", @users.to_csv(:only => :name) )
+  end
+  
+  def test_with_except
+    assert_equal( "age\n24\n21\n", @users.to_csv(:except => :name) )
+  end
+  
 end
