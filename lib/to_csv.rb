7ed0a3b913d840206da32b65cc1aea78123f60d6
Ary Djmal <arydjmal@gmail.com>
2010-02-16 13:48:39 -0500

    Use human_attribute_name for headers.

--- lib/to_csv.rb
+++ lib/to_csv.rb
@@ -1,28 +1,27 @@
 class Array
-  
   def to_csv(options = {})
     return '' if self.empty?
-    
-    attributes = self.first.attributes.keys.collect { |c| c.to_sym }
-    
+
+    klass      = self.first.class
+    attributes = self.first.attributes.keys.sort.map(&:to_sym)
+
     if options[:only]
       columns = Array(options[:only]) & attributes
     else
       columns = attributes - Array(options[:except])
     end
-        
+
     columns += Array(options[:methods])
-    
+
     return '' if columns.empty?
-    
+
     output = FasterCSV.generate do |csv|
-      csv << columns unless options[:headers] == false
+      csv << columns.map { |column| klass.human_attribute_name(column) } unless options[:headers] == false
       self.each do |item|
         csv << columns.collect { |column| item.send(column) }
       end
     end
-    
+
     output
   end
-  
 end
--- test/to_csv_test.rb
+++ test/to_csv_test.rb
@@ -1,15 +1,11 @@
-require 'test/unit'
-require 'rubygems'
-require 'fastercsv'
-require File.dirname(__FILE__) + '/../lib/to_csv'
-require File.dirname(__FILE__) + '/user_model'
+require File.dirname(__FILE__) + '/test_helper'
 
 class ToCsvTest < Test::Unit::TestCase
-
   def setup
-    @users = []
-    @users << User.new(:id => 1, :name => 'Ary', :age => 24)
-    @users << User.new(:id => 2, :name => 'Nati', :age => 21)
+    @users = [
+      User.new(:id => 1, :name => 'Ary', :age => 25),
+      User.new(:id => 2, :name => 'Nati', :age => 22)
+    ]
   end
 
   def test_with_empty_array
@@ -17,35 +13,34 @@ class ToCsvTest < Test::Unit::TestCase
   end
 
   def test_with_no_options
-    assert_equal( "id,name,age\n1,Ary,24\n2,Nati,21\n", @users.to_csv )
+    assert_equal( "Age,Id,Name\n25,1,Ary\n22,2,Nati\n", @users.to_csv )
   end
-  
+
   def test_with_no_headers
-    assert_equal( "1,Ary,24\n2,Nati,21\n", @users.to_csv(:headers => false) )
+    assert_equal( "25,1,Ary\n22,2,Nati\n", @users.to_csv(:headers => false) )
   end
-  
+
   def test_with_only
-    assert_equal( "name\nAry\nNati\n", @users.to_csv(:only => :name) )
+    assert_equal( "Name\nAry\nNati\n", @users.to_csv(:only => :name) )
   end
-  
+
   def test_with_empty_only
     assert_equal( "", @users.to_csv(:only => "") )
   end
-  
+
   def test_with_only_and_wrong_column_names
-    assert_equal( "name\nAry\nNati\n", @users.to_csv(:only => [:name, :yoyo]) )
+    assert_equal( "Name\nAry\nNati\n", @users.to_csv(:only => [:name, :yoyo]) )
   end
-  
+
   def test_with_except
-    assert_equal( "age\n24\n21\n", @users.to_csv(:except => [:id, :name]) )
+    assert_equal( "Age\n25\n22\n", @users.to_csv(:except => [:id, :name]) )
   end
-  
+
   def test_with_except_and_only_should_listen_to_only
-    assert_equal( "name\nAry\nNati\n", @users.to_csv(:except => [:id, :name], :only => :name) )
+    assert_equal( "Name\nAry\nNati\n", @users.to_csv(:except => [:id, :name], :only => :name) )
   end
-  
+
   def test_with_except
-    assert_equal( "id,name,age,is_old?\n1,Ary,24,false\n2,Nati,21,false\n", @users.to_csv(:methods => [:is_old?]) )
+    assert_equal( "Age,Id,Name,Is old?\n25,1,Ary,false\n22,2,Nati,false\n", @users.to_csv(:methods => [:is_old?]) )
   end
-  
 end
