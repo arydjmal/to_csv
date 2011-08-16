d7bae048cc011fe9705685a41b2085baae930095
Francis Fish <francis@francisfish.com>
2011-08-16 10:15:32 +0200

    Edited lib/to_csv.rb via GitHub

--- lib/to_csv.rb
+++ lib/to_csv.rb
@@ -1,27 +1,41 @@
 class Array
+  # This to_csv is borrowed and adapted from https://github.com/arydjmal/to_csv/blob/master/lib/to_csv.rb
+  # works like to_xml and whatever but also has preferred_order and column_names options
   def to_csv(options = {})
+  
     return '' if self.empty?
 
     klass      = self.first.class
     attributes = self.first.attributes.keys.sort.map(&:to_sym)
 
-    if options[:only]
-      columns = Array(options[:only]) & attributes
+    if options[:preferred_order]
+      columns = Array(options[:preferred_order])
     else
-      columns = attributes - Array(options[:except])
+      if options[:only]
+        columns = Array(options[:only]) & attributes
+      else
+        columns = attributes - Array(options[:except])
+      end
+      columns += Array(options[:methods])
     end
 
-    columns += Array(options[:methods])
-
     return '' if columns.empty?
+    
+    if options[:column_names]
+      column_names = Array(options[:column_names])
+    else
+      column_names = columns.map { |column| klass.human_attribute_name(column) } unless options[:headers] == false
+    end
 
-    output = FasterCSV.generate do |csv|
-      csv << columns.map { |column| klass.human_attribute_name(column) } unless options[:headers] == false
+    output = CSV.generate do |csv|
+      csv << column_names
       self.each do |item|
         csv << columns.collect { |column| item.send(column) }
       end
     end
 
     output
+
   end
+
 end
